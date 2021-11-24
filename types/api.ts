f6a765a6716290f07cfe02d4b7ffbf346982cc21
Pavel Srom <srompavel98@gmail.com>
2021-11-24 10:58:09 +0100

    fix(api-types): revert post type back

--- types/api.ts
+++ types/api.ts
@@ -1,4 +1,12 @@
 import { getAllUsersHandler } from 'backend/hello'
 import { ApiResponse } from './helpers'
 
+// dummy type for json placeholder
+export type Post = {
+  id: number
+  userId: number
+  title: string
+  body: string
+}
+
 export type User = ApiResponse<ReturnType<typeof getAllUsersHandler>>[number]

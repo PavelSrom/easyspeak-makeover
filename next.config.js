8cec9f1033ee49f5672a9f5e919c0261660934e7
Pavel Srom <srompavel98@gmail.com>
2021-11-24 01:13:28 +0100

    feat(app): make it pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require("next-pwa")
+const runtimeCaching = require("next-pwa/cache")
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+})

d538db1e77453a777647a1c3c729a64b016e3ef2
Pavel Srom <srompavel98@gmail.com>
2021-11-24 10:41:31 +0100

    feat: add prisma and its npm scripts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from 'utils/prisma-client'
 
-type Data = {
-  name: string
-}
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const users = await prisma.user.findMany()
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+        return res.send(users)
+      } catch (err) {
+        console.log(err)
+        return res.status(500).send({ err })
+      }
+
+    case 'POST':
+      try {
+        const { email } = req.body
+        if (!email)
+          return res.status(400).send({ message: 'Missing name or email' })
+
+        const newUser = await prisma.user.create({ data: { email } })
+
+        return res.status(201).send(newUser)
+      } catch (err) {
+        console.log(err)
+        return res.status(500).send({ err })
+      }
+    default:
+      return res.status(405).send({ message: 'Method not allowed' })
+  }
 }

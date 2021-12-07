73734574a7e3cce516da4c510dffe5b937d96a43
Sara <cph-sf131@cphbusiness.dk>
2021-12-07 19:31:07 +0100

    Add missing colours and font
    add
    
    
    add

--- ui/text/index.tsx
+++ ui/text/index.tsx
@@ -1,23 +1,29 @@
 import clsx from 'clsx'
 
-type Variant = 'h1' | 'h1_light' | 'h2' | 'body' | 'body2' | 'caption'
-type ReturnElement = 'h1' | 'h2' | 'p' | 'span'
+type Variant = 'h1' | 'h1_light' | 'h2' | 'h3' | 'h4' | 'body' | 'body2' | 'small' | 'caption'
+type ReturnElement = 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'small' | 'span'
 
 const elements: Record<Variant, ReturnElement> = {
   h1: 'h1',
   h1_light: 'h1',
   h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
   body: 'p',
   body2: 'p',
+  small: 'small',
   caption: 'span',
 }
 
 const classes: Record<Variant, string> = {
-  h1: 'text-2xl font-semibold',
+  h1: 'text-2xl font-regular',
   h1_light: 'text-2xl font-light',
   h2: 'text-xl font-semibold',
-  body: 'text-base font-light',
-  body2: 'text-sm font-light',
+  h3: 'text-xl font-regular',
+  h4: 'text-l font-semibold',
+  body: 'text-base font-light body',
+  body2: 'text-sm font-light body',
+  small: 'text-s font-light body',
   caption: 'text-xs font-light',
 }
 

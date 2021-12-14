7196547a5065811044cb6be6003a87efd9e18015
Patryk <cph-pc97@cphbusiness.dk>
2021-12-15 00:10:03 +0100

    prisma test setup

---
+++ prisma/test.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createNewMemberHandler } from 'backend/auth'
+import { getApiSession } from 'utils/get-api-session'
+import { prismaMock } from 'singleton'
+import { ApiSession } from 'types/helpers'
+
+test('should create a new member', async () => {
+    const user = {
+        email: "test@test.com",
+        id: "999",
+        password: "010101",
+        invitationSent: true,
+        clubId: "9",
+        createdAt: new Date("2021-12-14T22:49:25+00:00"),
+        pwResetToken: null,
+        pwResetExpiration: null
+    }
+    prismaMock.user.create.mockResolvedValue(user)
+
+    await expect(createNewMemberHandler()).resolves.toEqual({
+        id: "999",
+        email: "test@test.com",
+        invitationSent: true
+    })
+    }
+
+    }
+})
